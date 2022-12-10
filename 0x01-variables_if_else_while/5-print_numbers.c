18308ad73846117903a1dc04beaa14b549db38b8
amegahcharles1 <amegahcharlesisaiah1@gmail.com>
2022-12-10 01:00:42 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+
+	return (0);
+}
