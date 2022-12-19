307c47a53126018b33b9b84f406a77f263d330fd
amegahcharles1 <amegahcharlesisaiah1@gmail.com>
2022-12-19 01:08:55 -0800

    I can only go one way.  I have not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,23 @@
+#include"main.h"
+
 /**
- * rev_string - reverses a string
+ * print_rev - prints a string in reverse
  *
  * @s: string parameter input
  *
  * Return: Nothing
 */
 
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int l, i;
-	char ch;
+	int index;
 
-	/*find string length without null char*/
-	for (l = 0; s[l] != '\0'; ++l)
+	/*finds the length of string without null character*/
+	for (index = 0; s[index] != '\0'; ++index)
 		;
 
-	/*swap the string by looping to half the string*/
-	for (i = 0; i < l / 2; ++i)
-	{
-		ch = s[i];
-		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
-		s[l - 1 - i] = ch;
-	}
-
+	/*print char from the last index as you decrement*/
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+	_putchar('\n');
 }
