871bd6e59fdc42152d899081dd827285110b25b9
amegahcharles1 <amegahcharlesisaiah1@gmail.com>
2023-01-05 01:47:09 -0800

    Once an idea has taken hold of the brain

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,26 +1,23 @@
+#include "main.h"
+
 /**
- * _pow_recursion - a function that returns the power of @x by @y x^y
- *
- * Description: this method is good for speed, check this youtube video
- *   https://youtu.be/wAyrtLAeWvI?list=PL2_aWCzGMAwLz3g66WrxFGSXvSsvyfzCO
- *
- * @x: integer x
- * @y: integer y
- *
- * Return: the power of @x by @y
-*/
+ * _pow_recursion - Search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
+ */
+
 int _pow_recursion(int x, int y)
 {
-	int even;
 
-	if (y < 0)		/*if y is a negative number, return -1*/
+	if (y < 0)
 		return (-1);
-	else if (y == 0)	/*x to the power of 0 is 1*/
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
 		return (1);
-	else if (y % 2 == 0)	/*if y is an even number*/
-	{
-		even = _pow_recursion(x, y / 2);
-		return (even * even);
-	}
-	else			/*if y is odd*/
-		return (x * _pow_recursion(x, y - 1));
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
