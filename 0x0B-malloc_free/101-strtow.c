1ba1be45fd6a407156546dcc93083a04d5c1d7e5
amegahcharles1 <amegahcharlesisaiah1@gmail.com>
2023-01-10 12:59:12 -0800

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,25 +2,101 @@
 #include <stdlib.h>
 
 /**
- * wrdcnt - counts the number of words in a string
- * @s: string to count
+ * wordcount - get word count from string
+ *             without spaces
  *
- * Return: int of number of words
- */
-int wrdcnt(char *s)
-{
-	int i, n = 0;
+ * @str: string to count words present
+ *
+ * Return: The number of words
+*/
 
-	for (i = 0; s[i]; i++)
+int wordcount(char *str)
+{
+	int words = 0;
+
+	while (*str != '\0')
 	{
-		if (s[i] == ' ')
+		/*skip spaces*/
+		if (*str == ' ')
+			str++;
+		else
 		{
-			if (s[i + 1] != ' ' && s[i + 1] != '\0')
-				n++;
+			/*count words*/
+			while (*str != ' ' && *str != '\0')
+				str++;
+			words++;
 		}
-		else if (i == 0)
-			n++;
 	}
-	n++;
-	return (n);
+	return (words);
+}
+
+/**
+ * free_array - free arr[i]
+ *
+ * @ar: array to free
+ * @i: array[i]
+ *
+ * Return: nothing
+*/
+
+void free_array(char **ar, int i)
+{
+	if (ar != NULL && i != 0)
+	{
+		while (i >= 0)
+		{
+			free(ar[i]);
+			i--;
+		}
+		free(ar);
+	}
+}
+
+/**
+ * strtow - split a string to words
+ *
+ * @str: string to split.
+ *
+ * Return: NULL if it fails
+*/
+
+char **strtow(char *str)
+{
+	int i, s, j, str_l, word;
+	char **string;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	str_l = wordcount(str);
+	/*return null if str_l == 0 || new == NULL*/
+	string = malloc((str_l + 1) * sizeof(char *));
+	if (str_l == 0 || string == NULL)
+		return (NULL);
+
+	for (i = s = 0; i < str_l; i++)
+	{
+		for (word = s; str[word] != '\0'; word++)
+		{
+			if (str[word] == ' ')
+				s++;
+
+			if (str[word] != ' ' && (str[word + 1] == ' ' || str[word + 1] == '\0'))
+			{
+				string[i] = malloc((word - s + 2) * sizeof(char));
+				if (string[i] == NULL)
+				{
+					free_array(string, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+
+		for (j = 0; s <= word; s++, j++)
+			string[i][j] = str[s];
+		string[i][j] = '\0';
+	}
+	string[i] = NULL;
+	return (string);
 }
