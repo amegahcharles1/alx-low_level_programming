869394fdce9c33ec993c01d9ad10468064ae28bd
amegahcharles1 <amegahcharlesisaiah1@gmail.com>
2023-02-02 10:59:10 -0800

    100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,56 @@
 #include "main.h"
 
+#define CHAR_BITS 8 /*size of char*/
+#define INT_BITS (sizeof(unsigned long int) * CHAR_BITS)
+
 /**
- * clear_bit - Sets the value of a bit at a given index to 0.
- * @n: A pointer to the bit.
- * @index: The index to set the value at - indices start at 0.
+ * print_bin - prints binary rep of @n
  *
- * Return: If an error occurs - -1.
- *         Otherwise - 1.
- */
+ * @number: decimal value
+ *
+ * Return: nothing
+*/
+void print_bin(unsigned long int number)
+{
+	if (number >> 1)
+		print_bin(number >> 1);
+
+	putc((number & 1) ? '1' : '0', stdout);
+}
+
+/**
+ * clear_bit - sets the value of a bit to 1 at a given
+ *           index.
+ *
+ * @n: number
+ * @index: index to set the value of bit to 1
+ *
+ * Return: 1 if it worked OR -1 if it failed
+*/
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
+	unsigned long int mask = 1;
+	#ifdef DEBUG
+	unsigned long int n_before = *n;
+	#endif
+
+	/*if index is greater than size of n in binary coded decimal*/
+	if (index > INT_BITS)
 		return (-1);
 
-	*n &= ~(1 << index);
+	mask = ~(mask << index); /*create mask based on index position*/
+
+	*n = (*n & mask);
+
+	#ifdef DEBUG
+	printf("\n%ld in binary is ", n_before);
+	print_bin(n_before);
+	printf(" while NOT (~) of mask right shifted to index %d is ", index);
+	print_bin(mask);
+	printf(" our new value n in binary is ");
+	print_bin(*n);
+	printf("\n\n");
+	#endif
 
 	return (1);
 }
