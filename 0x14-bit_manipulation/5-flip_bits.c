b656fa6cd54af39c0b0e5a18dea2c09e5b2670aa
amegahcharles1 <amegahcharlesisaiah1@gmail.com>
2023-02-02 11:00:52 -0800

    101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,20 +1,65 @@
 #include "main.h"
 
 /**
- * flip_bits - returns hamming distance of two bit words
- * which is the number of bits you would need to flip to get
- * from one number to another
- * @n: the first bit word
- * @m: the second bit word
+ * print_bin - prints binary rep of @n
  *
- * Return: the hamming distance
- */
+ * @number: decimal value
+ *
+ * Return: nothing
+*/
+void print_bin(unsigned long int number)
+{
+	if (number >> 1)
+		print_bin(number >> 1);
+
+	putc((number & 1) ? '1' : '0', stdout);
+}
+
+/**
+ * flip_bits - a function that returns the number of bits you
+ *             would need to flip to get from one number to
+ *             another
+ *
+ * @n: decimal number
+ * @m: the other decimal number
+ *
+ * Return: number of bits to flip to get @m from @n
+*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int shift = (sizeof(n) * BYTE_LENGHT);
-	int dist = 0;
+	unsigned int diff_bits = 0;
+	unsigned long int diff;
 
-	while (shift--)
-		dist += (n >> shift & 1) != (m >> shift & 1);
-	return (dist);
+	/*xor n and b*/
+	diff = n ^ m;
+
+	#ifdef DEBUG
+	printf("\n(n) %ld and (m) %ld in binary is ", n, m);
+	print_bin(n);
+	printf(" ");
+	print_bin(m);
+	printf(" diff is %ld and in binary is ", diff);
+	print_bin(diff);
+	printf("\n");
+	#endif
+
+	/*keep shifting diff to right*/
+	do {
+		#ifdef DEBUG
+		printf("diff %ld before right shift ", diff);
+		print_bin(diff);
+		printf("\n");
+		#endif
+
+		diff_bits += (diff & 1);
+		diff >>= 1;
+
+		#ifdef DEBUG
+		printf("diff %ld after right shift ", diff);
+		print_bin(diff);
+		printf(" and diff_bit %d before leaving loop\n\n", diff_bits);
+		#endif
+	} while (diff > 0);
+
+	return (diff_bits);
 }
